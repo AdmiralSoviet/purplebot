c689a66a0e3ab8a1fae14dda746a92fa2cc280f7
AdmiralSoviet <gogodaddy27@gmail.com>
2019-01-24 02:00:34 +1100

    added website to !pb_info

--- pb_commands/info.js
+++ pb_commands/info.js
@@ -19,6 +19,7 @@ const cmd = {
         embed.setThumbnail(client.user.avatarURL);
         embed.setTitle("PurpleBot Information");
         embed.setDescription(`PurpleBot is a simple but diverse Discord Bot written in Node, built to run across multiple servers. If you like this bot, you can support further development of this bot by joining the community and recommending this bot to others.`);
+        embed.addField("Offical Website :page_facing_up:", "http://gnomed.me/purple", true);
         embed.addField("Source Code :computer:", "https://github.com/AdmiralSoviet/purplebot", true);
         embed.addField("Disclaimer :warning:", "Please be aware that this bot logs certain guild activity (mainly just command execution). If you want a copy of this guild's log-file, you can request it using the getlog command. (Mod Only)");
         embed.setTimestamp();
