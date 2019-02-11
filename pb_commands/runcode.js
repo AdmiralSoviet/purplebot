0507bf7f00317c58bc2c2c0fa086fb6c959cc75c
AdmiralSoviet <gogodaddy27@gmail.com>
2019-02-12 02:57:33 +1100

    added runcode command - super dev only

---
+++ pb_commands/runcode.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+const cmd = {
+    name: "runcode",
+    category: "General",
+    desc: "Runs the specified code. (Dev Only - dangerous)",
+    usage: "runcode",
+    hidden: true,
+    modonly: false,
+    devonly: true,
+    exec: function (opts = {}) {
+        const {
+            m = m,
+            content_clean = content_clean
+        } = opts;
+        m.delete();
+        console.log("[WARN] Running runcode command, super dangerous...");
+        m.author.send(`Result of running function: ${eval(content_clean)}`);
+    }
+}
+
+module.exports = cmd;
