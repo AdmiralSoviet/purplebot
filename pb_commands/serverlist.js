e2f49e7505066c667707e6e960d6b0056250efbe
AdmiralSoviet <gogodaddy27@gmail.com>
2019-07-18 15:01:18 +1000

    update

--- pb_commands/serverlist.js
+++ pb_commands/serverlist.js
@@ -21,8 +21,15 @@ const cmd = {
             embed.setColor("RANDOM");
             embed.addField("Server ID:", v.id, true);
             embed.addField("Owner ID:", v.ownerID, true);
-            embed.addField("Region:", v.region);
-            embed.addField("Members:", v.memberCount);
+            embed.addField("Region:", v.region, true);
+            embed.addField("Members:", v.memberCount, true);
+            embed.addField("More Information:", `
+            Created: ${v.createdAt}
+            Roles: ${v.roles.array().length}
+            Categories: ${v.channels.filter(x => x.type == "category").array().length}
+            Text Channels: ${v.channels.filter(x => x.type == "text").array().length}
+            Voice Channels: ${v.channels.filter(x => x.type == "voice").array().length}
+            `);
             embed.setTimestamp();
             m.channel.send(embed)
                 .then(message => message.delete(client.guilds.array().length * 20000));
