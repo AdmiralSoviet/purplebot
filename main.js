79c099c59769389dcce3f2ce55b85afb0e56bc7d
AdmiralSoviet <gogodaddy27@gmail.com>
2019-10-09 21:07:59 +1100

    meme2

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const Purple = (() => {
             return {
                 questions: [],
                 quotes: [],
-                guilds: {}
+                notmeme: {}
             };
         } else {
             return JSON.parse(fs.readFileSync("./pb_data/storage.json", "utf-8"));
