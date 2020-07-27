38a03934d3c98f6040e7f9d13e8dfc5c3f0e4415
AdmiralSoviet <gogodaddy27@gmail.com>
2020-07-27 21:22:28 +1000

    fix

--- pb_commands/more.js
+++ pb_commands/more.js
@@ -12,7 +12,7 @@ const cmd = {
     exec: function (opts = {}) {
         const {
             m = m,
-            client = client,
+                client = client,
                 music = music,
                 config = config,
                 purple = purple
@@ -38,11 +38,7 @@ const cmd = {
             list.setDescription(`This is a list of videos related to the last/current played song. You can use the **'${config.prefix}autoqueue'** command to play suggested songs automatically.`);
             ytdl.getBasicInfo(song.link)
                 .then((x) => {
-                    if(x.videoDetails)
-                        list.setThumbnail(x.videoDetails.thumbnail.thumbnails[x.videoDetails.thumbnail.thumbnails.length - 1].url);
-                    else
-                        console.log(x);
-                    console.log()
+                    list.setThumbnail(x.playerResponse.videoDetails.thumbnail.thumbnails[x.videoDetails.thumbnail.thumbnails.length - 1].url);
                     x.related_videos.slice(0, 5).forEach((v) => {
                         list.addField(v.title, `By *${v.author}*`);
                     });
